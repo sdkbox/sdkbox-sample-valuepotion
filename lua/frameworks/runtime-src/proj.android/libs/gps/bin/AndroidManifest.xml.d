20077d056fbc4b897a6a9df0a830db86a2b7b289
tinybearc <tinybearc@gmail.com>
2016-03-07 01:07:11 +0800

    remove unused files

--- lua/frameworks/runtime-src/proj.android/libs/gps/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Volumes/extend/jenkins/sdkbox_release/extern/plugins/googleplayservices/googleplayservices/plugin/gps/bin/AndroidManifest.xml : \
-/Volumes/extend/jenkins/sdkbox_release/extern/plugins/googleplayservices/googleplayservices/plugin/gps/AndroidManifest.xml \
